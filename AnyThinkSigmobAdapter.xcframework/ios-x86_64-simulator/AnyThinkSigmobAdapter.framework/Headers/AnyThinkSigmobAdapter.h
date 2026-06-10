20af1e4e31f77b60418a9ac5be38e9cb11ac74cf
turboyjtx <turboyjtx@gmail.com>
2026-06-10 08:22:48 +0800

    6.1.0: update SDKs

---
+++ AnyThinkSigmobAdapter.xcframework/ios-x86_64-simulator/AnyThinkSigmobAdapter.framework/Headers/AnyThinkSigmobAdapter.h
@@ -0,0 +1,16 @@
+//
+//  AnyThinkSigmobAdapter.h
+//  AnyThinkSigmobAdapter
+//
+//  Created by GUO PENG on 2025/4/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AnyThinkSigmobAdapter.
+FOUNDATION_EXPORT double AnyThinkSigmobAdapterVersionNumber;
+
+//! Project version string for AnyThinkSigmobAdapter.
+FOUNDATION_EXPORT const unsigned char AnyThinkSigmobAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnyThinkSigmobAdapter/PublicHeader.h>
